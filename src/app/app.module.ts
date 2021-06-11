01d8f11454999dd2e53a1c7b740c3015cfab4250
Vladzo <vashchukvlad17@gmail.com>
2021-06-12 00:06:53 +0300

    classwork4 not completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,22 @@ import { UsersComponent } from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
 import {UserService} from "./services/user.service";
 import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
-import {PostService} from "./services/post.service";
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     UserComponent,
-    PostsComponent,
-    PostComponent
+    UserDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [UserService, PostService ],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
